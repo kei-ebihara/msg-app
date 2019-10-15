1cf1162d8d4b8133422514b0b39640a2d00c6f62
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-10-15 16:47:47 +0900

    メッセージアプリ改良

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,10 @@ class MessagesController < ApplicationController
   def show
     @msg = params[:msg]
   end
+
+  def show2
+    @msg1 = params[:msg1]
+    @msg2 = params[:msg2]
+    @msg = "#{params[:msg1]}と#{params[:msg2]}を結合"
+  end
 end
